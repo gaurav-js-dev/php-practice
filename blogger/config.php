de0f57da782ce9f786b882d548c557fde778daab
Gaurav <gsharma.jai@salucro.com>
2021-08-25 00:32:42 +0530

    Updated error handing

--- blogger/config.php
+++ blogger/config.php
@@ -1,1 +1,14 @@
-5465454
+<?php
+
+/**
+ * Configuration settings
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'database name');
+define('DB_USER', 'username');
+define('DB_PASS', 'password');
+
+define('SMTP_HOST', 'mail.afdaexample.com');
+define('SMTP_USER', 'user@example.com');
+define('SMTP_PASS', 'secret');
